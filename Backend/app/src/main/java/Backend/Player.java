5044a0885fbc09bf36c82ecea1a5f1f9a308cebe
igbodani <igbodani14@gmail.com>
2021-10-20 02:49:18 -0600

    Addedd lombok dependencies, Built Classes

---
+++ Backend/app/src/main/java/Backend/Player.java
@@ -0,0 +1,26 @@
+package Backend;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+
+public class Player {
+    @Setter
+    @Getter
+    private String name;
+    @Setter
+    @Getter
+    private int accountBalance;
+    @Setter
+    @Getter
+    private int position;
+    @Setter
+    @Getter
+    private int netWorth;
+    //private ArrayList<Property> propertyOwned;
+    //private ArrayList<Card> cardDrawn;
+
+
+
+}
---
+++ Backend/app/src/main/java/Backend/Space.java
@@ -0,0 +1,24 @@
+package Backend;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class Space {
+
+    @Getter
+    private final String name;
+    @Getter
+    private final int location;
+
+
+    /**
+     * Constructor
+     * @param location The postion on the gameboard
+     * @param name Name of the space
+     */
+    public Space(String name, int location) {
+        this.name = name;
+        this.location = location;
+    }
+
+}
